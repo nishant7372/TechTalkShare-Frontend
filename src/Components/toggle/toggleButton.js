a2cb334783868e7ea447a7ae2b672c286f599f20
NishantSingh02 <nst7372@gmail.com>
2023-05-03 21:22:08 +0530

    toggle button added

---
+++ src/Components/toggle/toggleButton.js
@@ -0,0 +1,13 @@
+import styles from "./toggleButton.module.css";
+import { useState } from "react";
+export default function ToggleButton({ on, setOn }) {
+  const toggle = () => setOn((o) => !o);
+  return (
+    <div
+      className={`${styles["btn"]} ${styles[on ? "on" : "off"]}`}
+      onClick={toggle}
+    >
+      <span className={styles["pin"]} />
+    </div>
+  );
+}
