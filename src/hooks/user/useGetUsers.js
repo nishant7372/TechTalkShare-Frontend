3a1c127ed1a8ca5cafea1b7c751ba8b238367ced
NishantSingh02 <nst7372@gmail.com>
2023-05-03 21:22:52 +0530

    init commit

---
+++ src/hooks/user/useGetUsers.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import axiosInstance from "../axios/axiosInstance";
+
+export const useGetUsers = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const getUsers = async (id) => {
+    setError(null);
+    setIsPending(true);
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await axiosInstance.get(`/users/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!res) {
+        throw new Error("Unable to get users");
+      }
+      return res;
+    } catch (error) {
+      if (error.response) {
+        setError(error?.response?.data?.message || "An error occurred.");
+      } else if (error.request) {
+        setError("Network error. Please try again later.");
+      } else {
+        setError("An error occurred. Please try again later.");
+      }
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return { getUsers, error, isPending };
+};
