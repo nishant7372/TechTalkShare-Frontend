9323718dd73810048d2f4a2486f654e3e552e685
NishantSingh02 <nst7372@gmail.com>
2023-05-04 21:35:25 +0530

    updated

--- src/hooks/axios/axiosInstance.js
+++ src/hooks/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://prostore-backend.onrender.com",
-  timeout: 30000,
+  timeout: 30000, // 30 sec timeout
 });
 
 export default axiosInstance;
