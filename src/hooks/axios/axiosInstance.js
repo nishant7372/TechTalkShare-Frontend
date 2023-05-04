d0e7dabc961d01127f5a7e77454f13aa2cd210fa
NishantSingh02 <nst7372@gmail.com>
2023-05-04 21:32:46 +0530

    avatar safari browser width fixed

--- src/hooks/axios/axiosInstance.js
+++ src/hooks/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://prostore-backend.onrender.com",
-  timeout: 8000,
+  timeout: 30000,
 });
 
 export default axiosInstance;
