bf30bdbb3ea7c157d989042ab73d16d64179cd15
NishantSingh02 <nst7372@gmail.com>
2023-06-06 18:37:17 +0530

    scrape hooks added

---
+++ src/hooks/article/useLeetcodeScrape.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import axiosInstance from "./../axios/axiosInstance";
+
+export const useLeetcodeScrape = () => {
+  const [isPending, setIsPending] = useState(false);
+
+  const leetcodeScrape = async (params) => {
+    setIsPending(true);
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await axiosInstance.get(`/scrape`, {
+        params,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      return res
+        ? { ok: true, data: res.data }
+        : { error: "Unable to Download Article" };
+    } catch (err) {
+      let error = "";
+      if (err.response) {
+        error = err?.response?.data?.message || "An error occurred.";
+      } else if (err.request) {
+        error = "Network error. Please try again later.";
+      } else {
+        error = "An error occurred. Please try again later.";
+      }
+      return { error };
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return { leetcodeScrape, isPending };
+};
