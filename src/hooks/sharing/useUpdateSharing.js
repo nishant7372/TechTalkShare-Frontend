96136702fa92dbff61791118b1ed7a68fe5ab6a7
NishantSingh02 <nst7372@gmail.com>
2023-07-04 19:37:30 +0530

    hook added

---
+++ src/hooks/sharing/useUpdateSharing.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axiosInstance from "./../axios/axiosInstance";
+
+export const useUpdateSharing = () => {
+  const [isPending, setIsPending] = useState(false);
+
+  const updateSharing = async (id, updates) => {
+    setIsPending(true);
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await axiosInstance.patch(`/sharing/${id}`, updates, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      return res
+        ? { ok: "Permissions Updated" }
+        : { error: "Unable to update permissions" };
+    } catch (err) {
+      let error = "";
+      if (err.response) {
+        error = err?.response?.data?.message || "An error occurred.";
+      } else if (err.request) {
+        error = "Network error. Please try again later.";
+      } else {
+        error = "An error occurred. Please try again later.";
+      }
+      return { error };
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return { updateSharing, isPending };
+};
