17872ee98f95d7c438d6ec8f8a8cdcf3169baa63
NishantSingh02 <nst7372@gmail.com>
2023-07-04 19:38:17 +0530

    hook modified - application socket connection

--- src/hooks/socket/socketConnection.js
+++ src/hooks/socket/socketConnection.js
@@ -1,28 +1,33 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
-import { useMessageContext } from "../context/useMessageContext";
-import { useAuthContext } from "../context/useAuthContext";
+import { useDispatch } from "react-redux";
+import { setError, setSuccess } from "../../features/alertSlice";
+import { setActiveDownloads } from "../../features/downloadSlice";
+import { setSocketId } from "../../features/authSlice";
 
 export const useSocketConnection = () => {
-  const { dispatch: messageDispatch } = useMessageContext();
-  const { dispatch } = useAuthContext();
+  const dispatch = useDispatch();
 
   const getActivedownloads = (activeDownloads) => {
-    dispatch({ type: "ACTIVE_DOWNLOADS", payload: activeDownloads });
+    dispatch(setActiveDownloads(activeDownloads));
   };
 
   useEffect(() => {
-    const socket = io(process.env.REACT_APP_PROD_SERVER_URL);
+    const socket = io(process.env.REACT_APP_DEV_SERVER_URL);
     socket.emit("connected", { message: "Connected Successfully" });
-    socket.on("socketId", (res) =>
-      dispatch({ type: "SOCKETID", payload: res.socketId })
-    );
+    socket.on("socketId", (res) => {
+      dispatch(setSocketId(res.socketId));
+    });
 
     socket.emit("downloadStatus", getActivedownloads);
 
     socket.on("downloadStatus", (res) => {
-      messageDispatch({ type: res.type, payload: res.status });
-      dispatch({ type: "ACTIVE_DOWNLOADS", payload: res.activeDownloads });
+      if (res.type === "SUCCESS") {
+        dispatch(setSuccess(res.status));
+      } else if (res.type === "ERROR") {
+        dispatch(setError(res.status));
+      }
+      dispatch(setActiveDownloads(res.activeDownloads));
     });
 
     return () => {
