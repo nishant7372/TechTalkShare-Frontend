40604ae3091b2deb08d9e9131f4b0321dc3809d3
NishantSingh02 <nst7372@gmail.com>
2023-07-04 19:42:12 +0530

    Sharing permission feature added

---
+++ src/pages/article/view-owner/sharing.js
@@ -0,0 +1,165 @@
+import styles from "./sharing.module.css";
+
+import { useState } from "react";
+import { useEffect } from "react";
+import { Tooltip } from "react-tooltip";
+
+import { useFormatDate } from "../../../hooks/utils/useFormatDate";
+import { useUpdateSharing } from "../../../hooks/sharing/useUpdateSharing";
+import { useDeleteSharing } from "../../../hooks/sharing/useDeleteSharing";
+
+import { useDispatch } from "react-redux";
+import { setError, setSuccess } from "../../../features/alertSlice";
+
+import ToggleButton from "../../../Components/button/toggleButton";
+import SimpleButton from "../../../Components/button/simpleButton";
+import Spinner from "../../../Components/loading-spinners/spinner/spinner";
+
+
+export default function Sharing({ sharing, updated, updateSharings }) {
+  const { timeSince } = useFormatDate();
+
+  const dispatch = useDispatch();
+  
+  const { deleteSharing, isPending: deletePending } = useDeleteSharing();
+  const { updateSharing, isPending: updatePending } = useUpdateSharing();
+  
+  const [noChange, setNoChange] = useState(false);
+  const [editPermission, setEditPermission] = useState(sharing.editPermission);
+
+  const handleEditStatus = (res) => {
+    setEditPermission(res);
+  };
+
+  const handleSave = async () => {
+    const res = await updateSharing(sharing._id, {
+      ...(editPermission !== sharing.editPermission && { editPermission }),
+    });
+    if (res.ok) {
+      dispatch(setSuccess(res.ok));
+    } else if (res.error) {
+      dispatch(setError(res.error));
+    }
+    updateSharings();
+  };
+
+  const handleDelete = async () => {
+    const res = await deleteSharing(sharing._id);
+    if (res.ok) {
+      dispatch(setSuccess(res.ok));
+    } else if (res.error) {
+      dispatch(setError(res.error));
+    }
+    updateSharings();
+  };
+
+  useEffect(() => {
+    setNoChange(editPermission === sharing.editPermission);
+  }, [editPermission, sharing]);
+
+  return (
+    <div className={styles["sharing"]}>
+      <div className={styles["top-container"]}>
+        <div
+          className={styles["col1"]}
+          data-tooltip-id="name2-tooltip"
+          data-tooltip-content={sharing.sharedWith.name}
+          data-tooltip-place="top"
+          data-tooltip-variant="info"
+        >
+          @ {sharing.sharedWith.userName}
+        </div>
+        <Tooltip
+          id="name1-tooltip"
+          style={{
+            fontSize: "1.6rem",
+            backgroundColor: "orange",
+            padding: "0.4rem 0.8rem",
+          }}
+        />
+        <Tooltip
+          id="name2-tooltip"
+          style={{
+            fontSize: "1.6rem",
+            backgroundColor: "green",
+            padding: "0.4rem 0.8rem",
+          }}
+        />
+        <div className={styles["col2"]}>
+          <ToggleButton on={editPermission} setOn={handleEditStatus} />
+        </div>
+
+        <div className={styles["col3"]}>
+          {!updated && (
+            <div className={styles["h4"]}>
+              {timeSince(sharing.createdAt)} ago
+            </div>
+          )}
+          {updated && (
+            <div className={styles["h4"]}>
+              {timeSince(sharing.updatedAt)} ago
+            </div>
+          )}
+        </div>
+        <div className={styles["col4"]}>
+          {updatePending ? (
+            <div className={styles["disabled"]}>
+              <Spinner />
+            </div>
+          ) : (
+            <div
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content={!noChange ? "Save Changes" : null}
+              data-tooltip-place="top"
+              data-tooltip-variant="info"
+              style={{ display: "inline-block" }}
+            >
+              <SimpleButton
+                icon={<i className="fa-regular fa-floppy-disk"></i>}
+                content="&nbsp; Save"
+                disabled={noChange}
+                buttonStyle={{
+                  fontSize: "1.6rem",
+                  padding: "0.3rem 0.8rem",
+                  ...(noChange && { cursor: "not-allowed" }),
+                  ...(noChange && { backgroundColor: "#555" }),
+                  textAlign: "center",
+                }}
+                type="saveButton"
+                action={handleSave}
+              />
+            </div>
+          )}
+          <Tooltip
+            id="my-tooltip"
+            style={{
+              fontSize: "1.6rem",
+              backgroundColor: "orangered",
+              padding: "0.4rem 0.8rem",
+            }}
+          />
+        </div>
+        <div className={styles["col5"]}>
+          {deletePending ? (
+            <div className={styles["disabled"]}>
+              <Spinner />
+            </div>
+          ) : (
+            <SimpleButton
+              icon={<i className="fa-solid fa-user-slash"></i>}
+              content="&nbsp; Revoke"
+              buttonStyle={{
+                fontSize: "1.6rem",
+                padding: "0.3rem 0.8rem",
+                textWrap: "noWrap",
+                textAlign: "center",
+              }}
+              type="deleteButton"
+              action={handleDelete}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/article/view-owner/sharings.js
@@ -0,0 +1,180 @@
+import styles from "./sharings.module.css";
+
+import { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+
+import { useGetSharings } from "../../../hooks/sharing/useGetSharings";
+
+import { useDispatch } from "react-redux";
+import { setError } from "../../../features/alertSlice";
+
+import Sharing from "./sharing";
+import NotFound from "../../error/notFound";
+import ShareModal from "../components/modal/shareModal";
+import AnimatedButton from "../../../Components/button/animatedButton";
+import Loading from "../../../Components/loading-spinners/loading/loading";
+
+export default function Sharings() {
+  const { id } = useParams();
+
+  const { getSharings, isPending } = useGetSharings();
+
+  const [data, setData] = useState(null);
+  const [sharings, setSharings] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [activeFilter, setActiveFilter] = useState("Newest to Oldest");
+  const [openShareModal, setOpenShareModal] = useState(false);
+  const [showNotFound, setShowNotFound] = useState(false);
+  const dispatch = useDispatch();
+  const nodeRef = useRef(null);
+
+  const handleShare = () => {
+    setOpenShareModal(true);
+  };
+
+  const fetchSharings = async () => {
+    const res = await getSharings(id);
+    if (res.ok) setData(res.data.sharings);
+    else if (res.error) {
+      dispatch(setError(res.error.message));
+      if (res.error.status === 404) setShowNotFound(true);
+    }
+  };
+
+  const applySortedData = (dataToSort) => {
+    let sortedData;
+    if (activeFilter === "Recently Updated") {
+      sortedData = [...dataToSort].sort(
+        (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+      );
+    }
+    if (activeFilter === "Newest to Oldest") {
+      sortedData = [...dataToSort].sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    }
+    setSharings(sortedData);
+  };
+
+  useEffect(() => {
+    fetchSharings();
+  }, []);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const filteredData = data.filter(
+        ({ sharedWith, sharedBy }) =>
+          sharedWith.userName.startsWith(searchTerm) ||
+          sharedBy.userName.startsWith(searchTerm)
+      );
+      applySortedData(filteredData);
+    } else {
+      if (data) applySortedData(data);
+    }
+  }, [data, activeFilter, searchTerm]);
+
+  return (
+    <>
+      {isPending && <Loading action={"mainRead"} />}
+      {data && (
+        <div className={styles["sharing-container"]}>
+          <div className={styles["sharing-header"]}>
+            <div className="flex-row">
+              <div
+                className={`${styles["filterButton"]} ${
+                  activeFilter === "Recently Updated" ? styles["active"] : ""
+                }`}
+                onClick={() => setActiveFilter("Recently Updated")}
+              >
+                Recently Updated
+              </div>
+              <div
+                className={`${styles["filterButton"]} ${
+                  activeFilter === "Newest to Oldest" ? styles["active"] : ""
+                }`}
+                onClick={() => setActiveFilter("Newest to Oldest")}
+              >
+                Newest to Oldest
+              </div>
+            </div>
+            <div className="flex-row">
+              <div className={styles["searchBar"]}>
+                <input
+                  type="searchTerm"
+                  placeholder="🔍 Username"
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  value={searchTerm}
+                  required
+                  maxLength={50}
+                />
+              </div>
+
+              <AnimatedButton
+                content="+ Share"
+                buttonStyle={{
+                  fontSize: "1.6rem",
+                  height: "calc(3rem - 0.3rem - 2px)",
+                  padding: "0.15rem 0.8rem",
+                }}
+                action={() => handleShare()}
+                type="createBt"
+              />
+            </div>
+          </div>
+          <div>
+            <div className={styles["sharing-subheader"]}>
+              <div className={styles["top-container"]}>
+                <div className={styles["col1"]}>SharedWith</div>
+                <div className={styles["col2"]}>Edit</div>
+
+                <div className={styles["col3"]}>
+                  {activeFilter === "Recently Updated" && (
+                    <div className={styles["h4"]}>Updated At</div>
+                  )}
+                  {activeFilter === "Newest to Oldest" && (
+                    <div className={styles["h4"]}>Shared At</div>
+                  )}
+                </div>
+                <div className={styles["col4"]}>Changes</div>
+                <div className={styles["col5"]}>Access</div>
+              </div>
+            </div>
+          </div>
+          {sharings &&
+            sharings.map((sharing) => (
+              <Sharing
+                key={sharing._id}
+                sharing={sharing}
+                updated={activeFilter === "Recently Updated"}
+                updateSharings={fetchSharings}
+              />
+            ))}
+          {sharings && sharings.length === 0 && (
+            <div className={styles["no-sharings-found"]}>
+              No Sharing found. Click on Sharing to share this article.
+            </div>
+          )}
+          <div className={styles["sharing-footer"]}></div>
+        </div>
+      )}
+
+      <CSSTransition
+        in={openShareModal}
+        timeout={300}
+        nodeRef={nodeRef}
+        classNames="message"
+        unmountOnExit
+      >
+        <ShareModal
+          articleShare={id}
+          setOpenShareModal={setOpenShareModal}
+          nodeRef={nodeRef}
+          editPermission={true}
+          updateSharings={fetchSharings}
+        />
+      </CSSTransition>
+      {showNotFound && <NotFound />}
+    </>
+  );
+}
