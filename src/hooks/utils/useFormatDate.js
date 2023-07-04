a46d08ee4b86e77b8b62d638dbe708653dfd0b47
NishantSingh02 <nst7372@gmail.com>
2023-07-04 19:39:33 +0530

    data formatting and time formatting functions add

--- src/hooks/utils/useFormatDate.js
+++ src/hooks/utils/useFormatDate.js
@@ -1,4 +1,16 @@
 export const useFormatDate = () => {
+  const formatTime = (dateString) => {
+    const date = new Date(dateString);
+    const locale = navigator.locale;
+
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+    };
+
+    return new Intl.DateTimeFormat(locale, options).format(date) + "";
+  };
+
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const locale = navigator.locale;
@@ -49,5 +61,31 @@ export const useFormatDate = () => {
     return `${t} second${t === 1 ? "" : "s"}`;
   };
 
-  return { formatDate, timeSince };
+  const formatDate2 = (dateString) => {
+    const inputDate = new Date(dateString);
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    if (isSameDate(inputDate, today)) {
+      return "Today";
+    } else if (isSameDate(inputDate, yesterday)) {
+      return "Yesterday";
+    } else {
+      const day = inputDate.getDate().toString().padStart(2, "0");
+      const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
+      const year = inputDate.getFullYear().toString();
+      return `${day}/${month}/${year}`;
+    }
+  };
+
+  function isSameDate(date1, date2) {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }
+
+  return { formatTime, formatDate, timeSince, formatDate2 };
 };
