6000d77d317f2fc8e361a7c59b3ff960629c8023
NishantSingh02 <nst7372@gmail.com>
2023-07-31 10:02:26 +0530

    updated

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./../features/authSlice";
+import alertReducer from "./../features/alertSlice";
 import articleReducer from "./../features/articleSlice";
 import sharingReducer from "./../features/sharingSlice";
-import alertReducer from "./../features/alertSlice";
-import authReducer from "./../features/authSlice";
 import downloadReducer from "./../features/downloadSlice";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
+    alert: alertReducer,
     article: articleReducer,
     sharing: sharingReducer,
-    alert: alertReducer,
-    auth: authReducer,
     download: downloadReducer,
   },
 });
