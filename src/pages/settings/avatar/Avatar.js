5a94000c0efb421ee8ca452a6046062912444c85
NishantSingh02 <nst7372@gmail.com>
2023-09-18 00:09:16 +0530

    updated avatar hooks

--- src/pages/settings/avatar/Avatar.js
+++ src/pages/settings/avatar/Avatar.js
@@ -2,9 +2,11 @@ import styles from "./Avatar.module.css";
 
 import { useState } from "react";
 
-import { useUploadAvatar } from "../../../hooks/avatar/useUploadAvatar";
 import { useReadProfile } from "../../../hooks/user/userApis";
-import { useDeleteAvatar } from "../../../hooks/avatar/useDeleteAvatar";
+import {
+  useDeleteAvatar,
+  useUploadAvatar,
+} from "../../../hooks/avatar/avatarApis";
 
 import { useSelector } from "react-redux";
 
