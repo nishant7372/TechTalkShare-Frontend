28e9fdb152a20a75a81a31732fca988122ffc173
nishant-juno <nishant@onjuno.com>
2024-09-23 12:42:43 +0530

    minor fix

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,8 @@
 const apiConstants = {
-  BASE_URL: process.env.REACT_APP_DEV_SERVER_URL,
+  BASE_URL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_DEV_SERVER_URL
+      : process.env.REACT_APP_PROD_SERVER_URL,
 };
 
 export default apiConstants;
